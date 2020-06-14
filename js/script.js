2a520b491ce0ca2fa3a7143818485c341d19df3a
lizjack <65046262+lizjack@users.noreply.github.com>
2020-06-14 14:35:31 -0700

    First Initial Steps

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("name").focus();
+
+const otherTextBox = document.getElementById("other-title");
+otherTextBox.style.display='none';
+
+/** target the 'Other' input field and hide it initially
+ * make it appear when selected
+ * 
+ */
+
+ /** Update the "Color" Field to read "Please Select a t-shirt theme" 
+  * Hide the colors in the "Color" drop down menu.
+  * 
+ */
