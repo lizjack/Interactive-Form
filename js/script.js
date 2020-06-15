241d0a3865f8f866ec58e9f416ffacfa98f43278
lizjack <65046262+lizjack@users.noreply.github.com>
2020-06-14 18:35:55 -0700

    Added focus and textbox option
    Added focus on Name textbox and added Job Role textbox for "other" option

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 document.getElementById("name").focus();
 
 const otherTextBox = document.getElementById("other-title");
-otherTextBox.style.display='none';
+const choices = document.getElementById("title");
+otherTextBox.hidden = true;
 
 /** target the 'Other' input field and hide it initially
  * make it appear when selected
  * 
  */
+choices.addEventListener('change', () => {
+    if (choices.options[5].selected) {
+    otherTextBox.style.display ='block'; 
+    } else {
+        otherTextBox.hidden = true;
+    }
+});
 
  /** Update the "Color" Field to read "Please Select a t-shirt theme" 
   * Hide the colors in the "Color" drop down menu.
