f55188a5ad1a75aa75a6edad4bc72291e5d434c8
lizjack <65046262+lizjack@users.noreply.github.com>
2020-06-20 18:07:00 -0700

    Added Code Comments

--- js/script.js
+++ js/script.js
@@ -5,10 +5,7 @@ const otherTextBox = document.getElementById("other-title");
 const choices = document.getElementById("title");
 otherTextBox.hidden = true;
 
-/** target the 'Other' input field and hide it initially
- * make it appear when selected
- * 
- */
+// target the 'Other' input field and hide/appear as selected
 choices.addEventListener('change', (event) => {
     if (event.target.value === 'other') {
     otherTextBox.hidden = false; 
@@ -18,24 +15,27 @@ choices.addEventListener('change', (event) => {
 });
 
 /***** T-shirt Section *****/
- 
+
   //Hide Select Theme option in Design: menu
  const design = document.getElementById("design");
  design.options[0].style.display='none';
 
- //Hide colors in the Color: menu
  const colorMenu = document.getElementById("color");
  const shirtLabel = document.querySelector('label[for="color"]');
-shirtLabel.textContent='Please Select a t-shirt theme';
+//Create text prompting user to select a theme before seeig color options
+ shirtLabel.textContent='Please Select a t-shirt theme';
  //Create variables for the Color: menu options
 const shirtColors = colorMenu.children;
 const jsPuns = design[1];
 const heartShirt = design[2];
+//Initially hide color menu
 colorMenu.hidden = true;
 
+//Create eventListener to display corresponding shirt colors for shirt designs
 design.addEventListener('change', (event) => {
     for (var i = 0; i < shirtColors.length; i++) {
-      if (event.target.value === 'js puns') {
+    //if 'Js Puns' is selected, display shirts with 'JS Puns'
+        if (event.target.value === 'js puns') {
         shirtColors[0].selected = true;
         if (shirtColors[i].innerHTML.includes('JS Puns')) {
           shirtColors[i].style.display = '';
@@ -44,6 +44,7 @@ design.addEventListener('change', (event) => {
         } else {
           shirtColors[i].style.display = 'none';
         }
+    //if 'heart js' shirt is selected, display shirts that include '♥ JS'
       } else if (event.target.value === 'heart js') {
         shirtColors[3].selected = true;
           if (shirtColors[i].innerHTML.includes('♥ JS')) {
@@ -59,13 +60,15 @@ design.addEventListener('change', (event) => {
 
 /***** Activity Section *****/
 
-//create a variable to store the checkboxes
-const checkboxes = document.querySelectorAll('.activities input');
+//create a variable to store the checkboxes, activities section, cost
 const activitiesSection = document.querySelector('.activities');
+const checkboxes = document.querySelectorAll('.activities input');
 const activityCost = document.querySelectorAll('.data-cost');
 let priceTotalElement = document.createElement('textbox');
 let priceTotal = 0;
 
+//create eventListener to look for total cost of activities
+//eventListener also disables conflicting activities so user cannot register for two events at the same time
 document.querySelector('.activities').addEventListener('change', (e) => { 
     const clicked = e.target;
     const clickedCost = parseInt(clicked.getAttribute('data-cost'));
@@ -92,16 +95,17 @@ document.querySelector('.activities').addEventListener('change', (e) => {
 
 /***** Payment Section*****/
 
+//create payment menu variable get rid of "select payment method" option
 const paymentMenu = document.getElementById('payment');
 paymentMenu.removeChild(paymentMenu[0]);
-
+//create variables for payment methods and hide paypal/bitcoin instructions
 const creditCard = document.getElementById('credit-card');
 const paypal = document.getElementById('paypal');
 const bitcoin = document.getElementById('bitcoin');
 paypal.style.display = 'none';
 bitcoin.style.display = 'none';
-//creditCard.style.display = 'none';
 
+//create eventlistener to show and hide payment method instructions depending on if they are selected
 paymentMenu.addEventListener('change', (event) => {
     if (event.target.value === 'credit card') {
         creditCard.style.display = '';
@@ -121,12 +125,16 @@ paymentMenu.addEventListener('change', (event) => {
 
 /***** Form Validation Section *****/
 
+ //create name and email variables
 const name = document.querySelector("#name"); 
 const email = document.querySelector("#mail");
 
+//create a function that validates user name and alerts user if input is incompatible
  function nameValidation() {
     const nameValue = name.value; 
-    if (nameValue <= 0) {
+    const nameRegex = /^[a-z]+$/i;
+    const checkName = nameRegex.test(nameValue);
+    if (!checkName) {
       name.style.borderColor = "red";
       return false;  
     } else {
@@ -134,7 +142,7 @@ const email = document.querySelector("#mail");
         return true;
     }
 }
-
+//createa a function that validates user email and alerts user if input is incompatible
  function emailValidation() {
     const emailValue = email.value;
     const emailRegex = /^[^@]+@[^@.]+\.[a-z]+$/i ;
@@ -147,6 +155,8 @@ const email = document.querySelector("#mail");
         return true;
     }
 }
+
+//create a function that checks for at least one activity registration and alerts user is no activity is checked
     function activitiesValidation() {
     const activitiesBoxes = document.querySelectorAll('.activities input');
     const legend = document.querySelector('.activities legend');
@@ -154,15 +164,17 @@ const email = document.querySelector("#mail");
     for (let i = 0; i < activitiesBoxes.length; i ++) {
         if (activitiesBoxes[i].checked) {
             checkedBoxes += 1;
-        } if (checkedBoxes === 0) {
+        } 
+    }
+    if (checkedBoxes === 0) {
             legend.style.color = "red";
             return false;
         } if (checkedBoxes > 0) {
             return true;
         }
       }   
-    } 
-
+    
+//create a function that validates credit card input has 13-16 number characters
  function creditCardValidation() {
     const creditNumber = document.getElementById('cc-num');
     const creditInput = creditNumber.value;
@@ -177,6 +189,7 @@ const email = document.querySelector("#mail");
     }
 } 
 
+//create a function that validates zip code input has 5 number characters
 function zipCodeValidation() {
     const zipCode = document.getElementById('zip');
     const zipCodeValue = zipCode.value;
@@ -191,6 +204,7 @@ function zipCodeValidation() {
     }
 }
 
+//create a function that validates cvv input has 3 characters
 function cvvValidation() {
     const cvv = document.getElementById('cvv');
     const cvvValue = cvv.value;
@@ -205,6 +219,8 @@ function cvvValidation() {
     }
 }
 
+/** create an eventListener that checks to see if validation functions are true/false. 
+ * if validations functions are false, it prevents the form from being submitted. **/
  document.addEventListener('submit', (e) => {
     if (!nameValidation()) {
     e.preventDefault();
@@ -219,22 +235,25 @@ function cvvValidation() {
     if (!activitiesValidation()) {
     e.preventDefault();
     } 
-
+//Credit card/cvv/zipcode validation is on ly required if the credit card payment method is selected
     if (paymentMenu[0].selected) {
-    (!creditCardValidation()) 
-    e.preventDefault();
-    } 
-
-    if (paymentMenu[0].selected) {
-    (!cvvValidation()) 
+    creditCardValidation(); 
+    if(!creditCardValidation()) {
     e.preventDefault();
     }
+} 
 
     if (paymentMenu[0].selected) {
-    (!zipCodeValidation()) 
+    cvvValidation();
+    if (!cvvValidation()) {
+    e.preventDefault();
+    }
+}
+
+    if (paymentMenu[0].selected) {
+    zipCodeValidation(); 
+    if (!zipCodeValidation())  {
     e.preventDefault();
     } 
+}
 });
-
-//credit card validation not working
-//main conference needs to be checked for form to go thru
